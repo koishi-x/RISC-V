d5eb61353e8337770dc55b63a63adce0c9bcc789
koishi-x <2408336631@qq.com>
2023-06-26 17:00:46 +0800

    6/23 afternoon work, compile pass.

--- simulator.cpp
+++ simulator.cpp
@@ -10,12 +10,13 @@ constexpr unsigned FULL_BIT = -1;
 constexpr unsigned MAX_REGISTER_NUM = 32;
 constexpr unsigned MAX_BUFFER_SIZE = 32;
 
-inline void CHECK_YOUR_XXX () {puts("Something went wrong"); }
+inline void CHECK_YOUR_XXX (int x = 0) {printf("Something went wrong at %d\n", x); }
 inline unsigned getBit (unsigned x, int low, int high) {return (x >> low) & (FULL_BIT >> (31 - high + low)); }
 inline unsigned charToInt(char c) {return c <= '9' ? c - '0' : c - 'A' + 10; }
 inline unsigned sext(unsigned x, int high) {return (x >> high) & 1 ? x | (FULL_BIT << (high + 1)) : x; }
 
-bool flagEnd;
+bool flagEnd, flagEnd_new;
+bool predictFailFlag, predictFailFlag_new;
 unsigned pc, pc_new;
 unsigned reg[MAX_REGISTER_NUM], reg_new[MAX_REGISTER_NUM];
 unsigned RF[MAX_REGISTER_NUM], RF_new[MAX_REGISTER_NUM];
@@ -122,7 +123,7 @@ public:
                 else if (detailType == 0b010) type = LW;
                 else if (detailType == 0b100) type = LBU;
                 else if (detailType == 0b101) type = LHU;
-                else CHECK_YOUR_XXX();
+                else CHECK_YOUR_XXX(1);
                 break;
             case 0b0010011: //ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI
                 detailType = getBit(x, 12, 14);
@@ -135,7 +136,7 @@ public:
                         detailType = getBit(x, 25, 31);
                         if (detailType == 0b0000000) type = SRLI;
                         else if (detailType == 0b0100000) type = SRAI;
-                        else CHECK_YOUR_XXX();
+                        else CHECK_YOUR_XXX(2);
                     }
                 } else {    //ADDI, SLTI, SLTIU, XORI, ORI, ANDI
                     imm = sext(getBit(x, 20, 31), 11);
@@ -146,7 +147,7 @@ public:
                     else if (detailType == 0b100) type = XORI;
                     else if (detailType == 0b110) type = ORI;
                     else if (detailType == 0b111) type = ANDI;
-                    else CHECK_YOUR_XXX();
+                    else CHECK_YOUR_XXX(3);
                 }
                 break;
 
@@ -159,7 +160,7 @@ public:
                 else if (detailType == 0b101) type = BGE;
                 else if (detailType == 0b110) type = BLTU;
                 else if (detailType == 0b111) type = BGEU;
-                else CHECK_YOUR_XXX();
+                else CHECK_YOUR_XXX(4);
                 break;
 
             case 0b0100011: //SB, SH, SW
@@ -168,7 +169,7 @@ public:
                 if (detailType == 0b000) type = SB;
                 else if (detailType == 0b001) type = SH;
                 else if (detailType == 0b010) type = SW;
-                else CHECK_YOUR_XXX();
+                else CHECK_YOUR_XXX(5);
                 break;
             case 0b0110011: //
                 detailType = getBit(x, 25, 31);
@@ -177,7 +178,7 @@ public:
                     detailType = getBit(x, 12, 14);
                     if (detailType == 0b000) type = SUB;
                     else if (detailType == 0b101) type = SRA;
-                    else CHECK_YOUR_XXX();
+                    else CHECK_YOUR_XXX(6);
                 } else if (detailType == 0b000000) {
                     detailType = getBit(x, 12, 14);
                     if (detailType == 0b000) type = ADD;
@@ -188,8 +189,8 @@ public:
                     else if (detailType == 0b101) type = SRL;
                     else if (detailType == 0b110) type = OR;
                     else if (detailType == 0b111) type = AND;
-                    else CHECK_YOUR_XXX();
-                } else CHECK_YOUR_XXX();
+                    else CHECK_YOUR_XXX(7);
+                } else CHECK_YOUR_XXX(8);
         }
     }
     void init(const Instruction &obj) {
@@ -269,7 +270,21 @@ public:
     bool full() {
         return siz == MAX_BUFFER_SIZE;
     }
+    void clear() {
+        head = 1, tail = 0, siz = 0;
+    }
+    Buffer<T> &operator=(const Buffer<T> &obj) {
+        if (&obj == this) return *this;
+        head = obj.head;
+        tail = obj.tail;
+        siz = obj.siz;
+        for (int i = 0; i < MAX_BUFFER_SIZE; ++i) {
+            buffer[i] = obj.buffer[i];
+        }
+        return *this;
+    }
 };
+
 Buffer<Instruction> insQue, insQue_new;
 Buffer<ROB_node> ROB, ROB_new;
 Buffer<SL_node> SLB, SLB_new;
@@ -315,7 +330,7 @@ void initMemory() {
     while (~scanf("%s", s)) {
         if (s[0] == '@') {
             addr = 0;
-            for (int i = 1; s[i]; ++i) addr = addr * 10 + s[i] - '0';
+            for (int i = 1; s[i]; ++i) addr = ((addr << 4) | charToInt(s[i]));
         } else {
             mem.modify((charToInt(s[0]) << 4) | charToInt(s[1]), addr, 1);
             ++addr;
@@ -324,22 +339,31 @@ void initMemory() {
 }
 
 void readInstruction() {
+    /*
     //unsigned curInstruction = mem.query(pc, 4);
     static bool curState = 0;
     static unsigned x;
 
+
     if (!curState) {
         x = mem.query(pc, 4);
-        curState = 1;
+        curState = true;
         return;
     }
-    curState = 0;
+
+    curState = false;
+    */
+    if (predictFailFlag) {
+        insQue.clear();
+        insQue_new.clear();
+        return;
+    }
 
     Instruction curInstruction;
-    curInstruction.init(x);
+    curInstruction.init(mem.query(pc, 4));
 
     if (curInstruction.type == END) {
-        flagEnd = true;
+        flagEnd_new = true;
         return;
     }
     if (insQue.full()) {
@@ -366,6 +390,13 @@ void readInstruction() {
 }
 
 void issueInstruction() {
+
+    if (predictFailFlag) {
+        for (int i = 0; i < MAX_BUFFER_SIZE; ++i) {
+            RF[i] = RF_new[i] = 0;
+        }
+        return;
+    }
 
     if (insQue.empty()) return;
     if (ROB.full()) return;
@@ -415,6 +446,14 @@ void issueInstruction() {
 }
 
 void work_RS() {
+
+    if (predictFailFlag) {
+        for (int i = 0; i < MAX_BUFFER_SIZE; ++i) {
+            RS[i].busy = RS_new[i].busy = false;
+        }
+        return;
+    }
+
     int pos = -1;
     for (int i = 0; i < MAX_BUFFER_SIZE; ++i) {
         if (RS[i].busy && RS[i].qj == 0 && RS[i].qk == 0) {
@@ -547,7 +586,7 @@ void work_RS() {
             CBD_update(ROB_id, ROB_new[ROB_id].value);
             break;
         default:
-            CHECK_YOUR_XXX();
+            CHECK_YOUR_XXX(9);
     }
 
     /*
@@ -588,34 +627,173 @@ void work_RS() {
 }
 
 void work_SLB() {
-    static signed SLcycle = -1;
-    if (SLcycle != -1) {
+    static signed SLcycle = 0;
+    if (predictFailFlag) {
+        SLB.clear();
+        SLB_new.clear();
+        SLcycle = 0;
+        return;
+    }
 
+    if (SLcycle) {
+        if (--SLcycle == 0) {
+            SL_node tmp = SLB.front();
+            SLB_new.pop();
+            unsigned ROB_id = tmp.ROB_id;
+            ROB_new[ROB_id].ready = true;
+            switch (tmp.type) {
+                case LB:
+                    ROB_new[ROB_id].value = sext(mem.query(tmp.vj + tmp.imm, 1), 7);
+                    CBD_update(ROB_id, ROB_new[ROB_id].value);
+                    break;
+                case LH:
+                    ROB_new[ROB_id].value = sext(mem.query(tmp.vj + tmp.imm, 2), 15);
+                    CBD_update(ROB_id, ROB_new[ROB_id].value);
+                    break;
+                case LW:
+                    ROB_new[ROB_id].value = mem.query(tmp.vj + tmp.imm, 4); //no need to sign-extend in RV32I.
+                    CBD_update(ROB_id, ROB_new[ROB_id].value);
+                    break;
+                case LBU:
+                    ROB_new[ROB_id].value = mem.query(tmp.vj + tmp.imm, 1);
+                    CBD_update(ROB_id, ROB_new[ROB_id].value);
+                    break;
+                case LHU:
+                    ROB_new[ROB_id].value = mem.query(tmp.vj + tmp.imm, 2);
+                    CBD_update(ROB_id, ROB_new[ROB_id].value);
+                    break;
+                case SB:
+                    mem.modify(tmp.vk, tmp.vj + tmp.imm, 1);
+                    break;
+                case SH:
+                    mem.modify(tmp.vk, tmp.vj + tmp.imm, 2);
+                    break;
+                case SW:
+                    mem.modify(tmp.vk, tmp.vj + tmp.imm, 4);
+                    break;
+            }
+        }
     }
 
     if (SLB.empty()) return;
-    auto tmp = SLB.front();
+    SL_node tmp = SLB.front();
     if (isLoad(tmp.type)) {
-        if (tmp.qj) return;
-        SLcycle = 3;
+        if (tmp.qj == 0) {
+            SLcycle = 3;
+        }
     } else if (isStore(tmp.type)) {
-        if ()
+        if (tmp.qj == 0 && tmp.qk == 0 && tmp.ready) {
+            SLcycle = 3;
+        }
+    } else CHECK_YOUR_XXX(10);
+}
+
+int predictSuccess = 0, predictTot = 0;
+
+/*
+void clear() {
+    insQue.clear(); insQue_new.clear();
+    ROB.clear(); ROB_new.clear();
+    SLB.clear(); SLB_new.clear();
+    SLcycle = 0;
+    for (int i = 0; i < MAX_BUFFER_SIZE; ++i) RS[i].busy = RS_new[i].busy = false;
+    flagEnd_new = false;
+    for (int i = 0; i < MAX_BUFFER_SIZE; ++i) RF_new[i] = 0;
+
+}
+*/
+
+void work_ROB() {
+    if (predictFailFlag) {
+        ROB.clear();
+        ROB_new.clear();
+
+    }
+    if (ROB.empty()) return;
+    auto tmp = ROB.front();
+    unsigned ROB_id = ROB.front_id();
+    if (!tmp.ready) {
+        if (isStore(tmp.type)) {
+            for (int i = 0; i < MAX_BUFFER_SIZE; ++i) {
+                if (SLB[i].ROB_id == ROB_id) {
+                    SLB_new[i].ready = true;
+                }
+            }
+        }
+        return;
+    }
+    ROB_new.pop();
+    if (hasRD(tmp.type)) {
+        for (int i = 0; i < MAX_BUFFER_SIZE; ++i) {
+            if (RF[i] == ROB_id) {
+                RF_new[i] = 0;
+                CBD_update(ROB_id, tmp.value);
+                reg_new[i] = tmp.value;
+            }
+        }
+    }
+
+    if (isBranch(tmp.type)) {
+        ++predictTot;
+        if (tmp.predict == tmp.fact) {
+            ++predictSuccess;
+            FZYC_modify_id = (tmp.imm & 0xFFF);
+            if (FZYC[FZYC_modify_id] < 3) ++FZYC_new[FZYC_modify_id];
+        } else {
+            FZYC_modify_id = (tmp.imm & 0xFFF);
+            if (FZYC_modify_id > 0) --FZYC_new[FZYC_modify_id];
+            pc_new = tmp.fact ? tmp.next_pc : tmp.pc + 4;
+            predictFailFlag_new = true;
+        }
+        /*
+        if (tmp.type == JAL) {
+            ++predictSuccess;
+            FZYC_modify_id = (tmp.imm & 0xFFF);
+            if (FZYC[FZYC_modify_id] < 3) ++FZYC_new[FZYC_modify_id];
+        } else if (tmp.type == JALR) {
+            predictFail = true;
+            FZYC_modify_id = (tmp.imm & 0xFFF);
+            if (FZYC_modify_id > 0) --FZYC_new[FZYC_modify_id];
+        } else {
+            if ()
+        }*/
     }
 }
 
-void work_ROB() {
 
+void updateAll() {
+    if (predictFailFlag) {
+        predictFailFlag = false;
+        return;
+    }
+    insQue = insQue_new;
+    ROB = ROB_new;
+    SLB = SLB_new;
+    pc = pc_new;
+    for (int i = 0; i < MAX_BUFFER_SIZE; ++i) RS[i] = RS_new[i], RF[i] = RF_new[i], reg[i] = reg_new[i];
+    flagEnd = flagEnd_new;
+    flagEnd_new = false;
+    predictFailFlag = predictFailFlag_new;
+    predictFailFlag_new = false;
 }
 
 int main() {
+    freopen("sample.data", "r", stdin);
+
     initMemory();
     flagEnd = false;
     pc = 0;
     while (true) {
+        //posedge
         readInstruction();
         issueInstruction();
         work_RS();
         work_SLB();
         work_ROB();
+
+        //negedge
+        updateAll();
+        if (flagEnd && insQue.empty() && ROB.empty() && !predictFailFlag) break;
     }
+    std::cout << (reg[10] & 255u);
 }
